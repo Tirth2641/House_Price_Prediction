45b2a20ae65b8057bd59a4cf1500c081774dc8c9
Patel Tirth <107361516+Tirth039@users.noreply.github.com>
2024-07-14 13:47:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split
-from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error
+from scikit-learn.model_selection import train_test_split
+from scikit-learn.linear_model import LinearRegression
+from scikit-learn.metrics import mean_squared_error
 import joblib
 import streamlit as st
 import numpy as np
